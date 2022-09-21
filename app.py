0b45c6dc7ef0616c2f4cfacad7b08ee394550b1e
anil-kumarhr <111352303+anil-kumarhr@users.noreply.github.com>
2022-09-22 00:29:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -29,7 +29,13 @@ def home():
     data1 = request.form['a']
     data2 = request.form['b']
     data3 = request.form['c']
-    arr = np.array([[data1, data2, data3]])
+    data4 = request.form['d']
+    data5 = request.form['e']
+    data6 = request.form['f']
+    data7 = request.form['g']
+    data8 = request.form['h']
+    data9 = request.form['i']
+    arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8, data9]])
     pred = model.predict(arr)
     return render_template('after.html',data=pred)
 
